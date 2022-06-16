5df1daca67b52129d0a88716762a83970219184a
John Jacques <john.jacques@intel.com>
2022-06-16 10:14:27 -0500

    Add the Ability to Issue CCC Commands
    Signed-off-by: John Jacques <john.jacques@intel.com>

--- i3ctransfer.c
+++ i3ctransfer.c
@@ -21,11 +21,12 @@
 
 #define VERSION "0.1"
 
-const char *sopts = "d:a:r:w:hv";
+const char *sopts = "d:c:r:w:thv";
 static const struct option lopts[] = {
 	{"device",		required_argument,	NULL,	'd' },
 	{"read",		required_argument,	NULL,	'r' },
 	{"write",		required_argument,	NULL,	'w' },
+	{"command",             required_argument,      NULL,   'c' },
 	{"tocwa",               no_argument,            NULL,   't' },
 	{"help",		no_argument,		NULL,	'h' },
 	{"version",		no_argument,		NULL,	'v' },
@@ -36,13 +37,20 @@ static void print_usage(const char *name)
 {
 	fprintf(stderr,
 		"  usage: %s options...\n"
- 		"   note: -r and -w are exclusive.  One, and only one, must be specified.\n",
+ 		"   note: -r, -w, and -c are exclusive.  One, and only one, must be specified.\n",
 		name);
 	fprintf(stderr,
 		"options:\n");
 	fprintf(stderr,
 		"    -d --device       <device>\n"
 		"        REQUIRED: device: Device entry to use.\n");
+	fprintf(stderr,
+		"    -c --ccc [read]   <id>:r:<address>:<length>[:<file>]\n"
+		"    -c --ccc [write]  <id>:w:<address>:<data>|<file>\n"
+		"           type: CCC code.\n"
+		"        address: Slave address.\n"
+		"           data: Write data.\n"
+		"           file: File containing data to write.\n");
 	fprintf(stderr,
 		"    -r --read         <type>:<address>:<length>[:<file>]\n"
 		"           type: i2c or i3c.\n"
@@ -67,13 +75,143 @@ static void print_usage(const char *name)
 		"        Output the version number and exit\n");
 }
 
-static int r_args_to_xfer(struct i3c_ioc_priv_xfer *xfer, char *arg)
+static int c_args_to_xfer(struct i3c_tools_ioctl *xfer, char *arg)
+{
+	char *data_ptrs[256];
+	int len, i = 0;
+	FILE *input;
+	char *tmp;
+
+	/**
+	 * <command>:r|w:<address>:<data>|<file>
+	 */
+
+	memset(xfer, 0, sizeof(*xfer));
+	xfer->type = I3C_TOOLS_CCC;
+
+	/* Command */
+
+	tmp = strtok(arg, ":");
+
+	if (!tmp) {
+		fprintf(stderr, "%s failed at %d\n", __func__, __LINE__);
+		return -1;
+	}
+
+	xfer->ccc = strtol(tmp, NULL, 0);
+
+	if (xfer->ccc < 0 || xfer->ccc > 0xff) {
+		fprintf(stderr, "<command> must be 0x00...0xff\n");
+		return -1;
+	}
+
+	/* Read or Write */
+
+	tmp = strtok(NULL, ":");
+
+	if (!tmp) {
+		fprintf(stderr, "%s failed at %d\n", __func__, __LINE__);
+		return -1;
+	}
+
+	if (strncmp("r", tmp, strlen("r")) == 0) {
+		xfer->rnw = 1;
+	} else if (strncmp("w", tmp, strlen("w")) == 0) {
+		xfer->rnw = 0;
+	} else {
+		fprintf(stderr,
+			"The second field of a CCC command must be either r (read) or w (write).\n");
+		return -1;
+	}
+
+	/* Address */
+
+	tmp = strtok(NULL, ":");
+
+	if (!tmp) {
+		fprintf(stderr, "%s failed at %d\n", __func__, __LINE__);
+		return -1;
+	}
+
+	xfer->addr = strtol(tmp, NULL, 0);
+
+	if (xfer->rnw) {
+		/* Allocate a buffer for the read. */
+
+		tmp = (char *)calloc(xfer->len, sizeof(uint8_t));
+
+		if (!tmp) {
+			fprintf(stderr,
+				"%s failed at %d\n", __func__, __LINE__);
+
+			return -1;
+	}
+
+	xfer->data = (uintptr_t)tmp;
+	} else {
+		/**
+		 * Allocate and fill a buffer with data.  The data can be
+		 * given on the command line as a comma seperated list of
+		 * values (0xnn,0xmm,etc.) OR a binary file.
+		 */
+
+		tmp = strtok(NULL, ":");
+		input = fopen(tmp, "rb");
+
+		if (input) {
+			fseek(input, 0L, SEEK_END);
+			xfer->len = ftell(input);
+			rewind(input);
+
+			tmp = (char *)calloc(xfer->len, sizeof(uint8_t));
+
+			if (!tmp) {
+				fprintf(stderr,
+					"%s failed at %d\n",
+					__func__, __LINE__);
+
+				return -1;
+			}
+
+			fread(tmp, sizeof(char *), xfer->len, input);
+			xfer->data = (uintptr_t)tmp;
+			fclose(input);
+		} else {
+			data_ptrs[i] = strtok(tmp, ",");
+
+			while (data_ptrs[i] && i < 255)
+				data_ptrs[++i] = strtok(NULL, ",");
+
+			tmp = (char *)calloc(i, sizeof(uint8_t));
+
+			if (!tmp) {
+				fprintf(stderr,
+					"%s failed at %d\n",
+					__func__, __LINE__);
+
+				return -1;
+			}
+
+			for (len = 0; len < i; len++)
+				tmp[len] = (uint8_t)strtol(data_ptrs[len],
+							   NULL, 0);
+
+			xfer->len = len;
+			xfer->data = (uintptr_t)tmp;
+		}
+	}
+
+	return 0;
+}
+
+static int r_args_to_xfer(struct i3c_tools_ioctl *xfer, char *arg)
 {
 	char *tmp;
 	int len;
 
 	memset(xfer, 0, sizeof(*xfer));
 	xfer->rnw = 1;
+	xfer->type = I3C_TOOLS_PRIV_XFER;
 
 	/* Type */
 	tmp = strtok(arg, ":");
@@ -134,7 +272,7 @@ static int r_args_to_xfer(struct i3c_ioc_priv_xfer *xfer, char *arg)
 	return 0;
 }
 
-static int w_args_to_xfer(struct i3c_ioc_priv_xfer *xfer, char *arg)
+static int w_args_to_xfer(struct i3c_tools_ioctl *xfer, char *arg)
 {
 	char *data_ptrs[256];
 	int len, i = 0;
@@ -147,6 +285,7 @@ static int w_args_to_xfer(struct i3c_ioc_priv_xfer *xfer, char *arg)
 
 	memset(xfer, 0, sizeof(*xfer));
 	xfer->rnw = 0;
+	xfer->type = I3C_TOOLS_PRIV_XFER;
 
 	/* Type */
 
@@ -228,7 +367,7 @@ static int w_args_to_xfer(struct i3c_ioc_priv_xfer *xfer, char *arg)
 	return 0;
 }
 
-static int handle_read(char *arg, struct i3c_ioc_priv_xfer *xfer)
+static int handle_read(char *arg, struct i3c_tools_ioctl *xfer)
 {
 	char *filename;
 	FILE *output;
@@ -268,7 +407,7 @@ static int handle_read(char *arg, struct i3c_ioc_priv_xfer *xfer)
 
 int main(int argc, char *argv[])
 {
-	struct i3c_ioc_priv_xfer *xfers;
+	struct i3c_tools_ioctl *xfers;
 	char **xfers_args;
 	int file, ret, opt, i;
 	int nxfers = 0;
@@ -309,7 +448,7 @@ int main(int argc, char *argv[])
 	if (file < 0)
 		exit(EXIT_FAILURE);
 
-	xfers = (struct i3c_ioc_priv_xfer *)calloc(nxfers, sizeof(*xfers));
+	xfers = (struct i3c_tools_ioctl *)calloc(nxfers, sizeof(*xfers));
 	if (!xfers)
 		exit(EXIT_FAILURE);
 
@@ -322,6 +461,19 @@ int main(int argc, char *argv[])
 
 	while ((opt = getopt_long(argc, argv, sopts, lopts, NULL)) != EOF) {
 		switch (opt) {
+		case 'c':
+			xfers_args[nxfers] = malloc(strlen(optarg) + 1);
+			strcpy(xfers_args[nxfers], optarg);
+
+			if (c_args_to_xfer(&xfers[nxfers], optarg)) {
+				fprintf(stderr, "c_args_to_xfer() failed!\n");
+				ret = EXIT_FAILURE;
+				goto err_free;
+			}
+
+			nxfers++;
+			break;
+			break;
 		case 'r':
 			xfers_args[nxfers] = malloc(strlen(optarg) + 1);
 			strcpy(xfers_args[nxfers], optarg);
@@ -353,7 +505,7 @@ int main(int argc, char *argv[])
 		(xfers[nxfers]).tocwa = tocwa;
 	}
 
-	if (ioctl(file, I3C_IOC_PRIV_XFER(nxfers), xfers) < 0) {
+	if (ioctl(file, I3C_TOOLS_TYPE(nxfers), xfers) < 0) {
 		fprintf(stderr,
 			"Error: transfer failed: %s\n", strerror(errno));
 		ret = EXIT_FAILURE;
--- include/i3c/i3cdev.h
+++ include/i3c/i3cdev.h
@@ -13,37 +13,47 @@
 
 #define VERSION "0.1"
 
-/* IOCTL commands */
-#define I3C_DEV_IOC_MAGIC	0x07
+/* IOCTL Defines */
+
+#define I3C_TOOLS_IOC_MAGIC 0x07
+
+enum i3c_tools_ioctl_type {
+	I3C_TOOLS_PRIV_XFER,
+	I3C_TOOLS_CCC
+};
 
 /**
- * struct i3c_ioc_priv_xfer - I3C SDR ioctl private transfer
- * @data: Holds pointer to userspace buffer with transmit data.
- * @len: Length of data buffer buffers, in bytes.
- * @addr: The address of the I2C slave.
+ * struct i3c_tools_ioctl - I2C/I3C Private Transfer OR CCC Command
+ * @type: Indicate the type (private transfer or CCC command).
+ * @data: Pointer to userspace buffer with transmit/receive data.
+ * @len: Length of the buffer, in bytes.
+ * @addr: The address of the slave.
  * @offset: For combo transfers.
  * @combo: combo transfer.
  * @i2cni3c: I2C slave.
- * @rnw: encodes the transfer direction. true for a read, false for a write
- * @tocwa: always end with TOC=1
+ * @rnw: Transfer direction. true for a read, false for a write.
+ * @ccc: The CCC command ID.
+ * @tocwa: Always end with TOC=1.
  */
 
-struct i3c_ioc_priv_xfer {
+struct i3c_tools_ioctl {
+	enum i3c_tools_ioctl_type type;
 	__u64 data;
 	__u16 len;
-	__u8  addr;	     /* slave address for i2c transfers */
-	__u16 offset;	     /* for combo transfers only */
-	__u8  combo;	     /* combo, instead of regular, transfer */
+	__u8  addr;
+	__u16 offset;
+	__u8  combo;
 	__u8  i2cni3c;
 	__u8  rnw;
+	__u8  ccc;
 	__u8  tocwa;
 };
 
-#define I3C_PRIV_XFER_SIZE(N)	\
-	((((sizeof(struct i3c_ioc_priv_xfer)) * (N)) < (1 << _IOC_SIZEBITS)) \
-	? ((sizeof(struct i3c_ioc_priv_xfer)) * (N)) : 0)
+#define I3C_TOOLS_SIZE(N)	\
+	((((sizeof(struct i3c_tools_ioctl)) * (N)) < (1 << _IOC_SIZEBITS)) \
+	 ? ((sizeof(struct i3c_tools_ioctl)) * (N)) : 0)
 
-#define I3C_IOC_PRIV_XFER(N)	\
-	_IOC(_IOC_READ|_IOC_WRITE, I3C_DEV_IOC_MAGIC, 30, I3C_PRIV_XFER_SIZE(N))
+#define I3C_TOOLS_TYPE(N)	\
+	_IOC(_IOC_READ|_IOC_WRITE, I3C_TOOLS_IOC_MAGIC, 30, I3C_TOOLS_SIZE(N))
 
 #endif
